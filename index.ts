dfd58e682dd5cb776b087602e1c4ffe12501be69
Nkosi <mhlopenkosikhona@gmail.com>
2025-09-18 09:38:35 +0200

    Set up Prettier

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import concurrently from 'concurrently'; //import a function from a module
+import concurrently from "concurrently"; //import a function from a module
 
 concurrently([
-    {
-        name: "server",
-        command: "bun run dev",
-        cwd: "packages/server",
-        prefixColor: "cyan"
-    },
-    {
-        name: "client",
-        command: "bun run dev",
-        cwd: "packages/client",
-        prefixColor: "green"
-    }
-])
+  {
+    name: "server",
+    command: "bun run dev",
+    cwd: "packages/server",
+    prefixColor: "cyan",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
     fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
   }, []);
 
   return (
     <div className="p-4">
-  <p className="font-bold text-4xl">{message}</p>
-  <Button>Click Me</Button>
-  </div>
-);
+      <p className="font-bold text-4xl">{message}</p>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
